a3afb7f8088a4eaee468b2cf97f5020e95f0bb12
sayak53 <sayakchakraborty928@gmail.com>
2024-10-14 19:39:44 +0530

    revised spred(...)operator

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const doubleIt = (num) => num*2
-console.log(doubleIt(5))
+// const doubleIt = (num) => num*2
+// console.log(doubleIt(5))
 
-const doMath = (x,y,z) => {
-    let sum = x+y;
-    sum -= z
-    return sum
-}
+// const doMath = (x,y,z) => {
+//     let sum = x+y;
+//     sum -= z
+//     return sum
+// }
 
-console.log(doMath(20,40,30))
+// console.log(doMath(20,40,30))
+
+const numbers = [34,57,8,9,54,45]
+const newNumbers=[...numbers]
+
+numbers.push(94)
+numbers.push(44)
+numbers.push(98)
+
+console.log(numbers)
+console.log(newNumbers)
