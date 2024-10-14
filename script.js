604ca14637a2d23e1d5ec006706ab3c3e225b4a4
sayak53 <sayakchakraborty928@gmail.com>
2024-10-14 19:51:45 +0530

    rrevised array methods

--- script.js
+++ script.js
@@ -9,12 +9,24 @@
 
 // console.log(doMath(20,40,30))
 
-const numbers = [34,57,8,9,54,45]
-const newNumbers=[...numbers]
+// const numbers = [34,57,8,9,54,45]
+// const newNumbers=[...numbers]
 
-numbers.push(94)
-numbers.push(44)
-numbers.push(98)
+// numbers.push(94)
+// numbers.push(44)
+// numbers.push(98)
 
-console.log(numbers)
-console.log(newNumbers)
+// console.log(numbers)
+// console.log(newNumbers)
+
+const heroes =[
+    {name:'batman',profession:'killing',dialouge:'i am vengance',killCount:50000},
+    {name:'superman',profession:'saving',dialouge:'not impresssed',killCount:500},
+    {name:'loki',profession:'killing',dialouge:'i am loki of asgard',killCount:10000000},
+    {name:'ironman',profession:'saving',dialouge:'i am ironman',killCount:800}
+]
+
+// heroes.forEach(hero => console.log(hero.name))
+// heroes.map(hero => console.log(hero.dialouge))
+const dgHeroes = heroes.find(hero => hero.killCount > 10000)
+console.log(dgHeroes)
